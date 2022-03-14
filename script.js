d2be3d483ce9ab9a90e8e3a40243ea462a6bba7a
sidramwaseem <sidraw24@gmail.com>
2022-03-15 01:01:19 +0500

    Updated JavaScript

---
+++ script.js
@@ -0,0 +1,17 @@
+function all_projects() {
+  document.getElementById("project1").style.display = "block";
+  document.getElementById("project2").style.display = "block";
+  document.getElementById("project3").style.display = "block";
+}
+
+function boot() {
+  document.getElementById("project1").style.display = "block";
+  document.getElementById("project2").style.display = "none";
+  document.getElementById("project3").style.display = "block";
+}
+
+function html() {
+  document.getElementById("project1").style.display = "none";
+  document.getElementById("project2").style.display = "block";
+  document.getElementById("project3").style.display = "none";
+}
